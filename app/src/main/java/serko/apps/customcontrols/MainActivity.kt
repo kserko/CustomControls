fb1c5823b3ccae786f845d1eabb847b532247fb6
serko <10026904+kserko@users.noreply.github.com>
2023-01-06 11:00:42 +0000

    Refactor and minor cleanup

--- app/src/main/java/serko/apps/customcontrols/MainActivity.kt
+++ app/src/main/java/serko/apps/customcontrols/MainActivity.kt
@@ -6,12 +6,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -31,10 +32,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import serko.apps.customcontrols.ui.theme.CustomControlsTheme
-import kotlin.math.ceil
 import kotlin.math.floor
 
-@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,44 +49,13 @@ class MainActivity : ComponentActivity() {
                             .fillMaxSize(),
 
                         ) {
-
-                        Box(
-                            modifier = Modifier
-                                .height(500.dp)
-                                .fillMaxWidth()
-                                .weight(0.8f)
-                        ) {
-                            RadialTemperatureControl(temperature = temperature.value)
-                        }
-
-                        Row(
+                        RadialTemperatureDisplay(temperature = temperature.value)
+                        TemperatureControls(
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .weight(0.8f),
-                            horizontalArrangement = Arrangement.SpaceBetween
-                        ) {
-                            IconButton(onClick = {
-                                if (temperature.value.isNotAtMinimum) {
-                                    temperature.value.targetTemperature -= 0.5f
-                                }
-                            }) {
-                                Icon(
-                                    painterResource(R.drawable.ic_minus),
-                                    contentDescription = null
-                                )
-                            }
-                            IconButton(onClick = {
-                                if (temperature.value.isNotAtMaximum) {
-                                    println("target ${temperature.value.targetTemperature+0.5}")
-                                    temperature.value.targetTemperature += 0.5f
-                                }
-                            }) {
-                                Icon(
-                                    painterResource(R.drawable.ic_plus),
-                                    contentDescription = null
-                                )
-                            }
-                        }
+                                .offset(y = (-100).dp), // move the controls closer to the display
+                            temperature = temperature.value
+                        )
                     }
                 }
             }
@@ -95,29 +63,57 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-data class Temperature(
-    var targetTemperature: Float = 19f,
-    val minTemperature: Float = 12f,
-    val maxTemperature: Float = 28f,
-    val increments: Float = 0.5f
+@Composable
+private fun TemperatureControls(
+    modifier: Modifier,
+    temperature: Temperature
 ) {
-    val isNotAtMinimum get() = targetTemperature != minTemperature
-    val isNotAtMaximum get() = targetTemperature != maxTemperature
-
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        IconButton(onClick = {
+            if (temperature.isNotAtMinimum) {
+                temperature.targetTemperature -= 0.5f
+            }
+        }) {
+            Icon(
+                painterResource(R.drawable.ic_minus),
+                contentDescription = null
+            )
+        }
+        IconButton(onClick = {
+            if (temperature.isNotAtMaximum) {
+                println("target ${temperature.targetTemperature + 0.5}")
+                temperature.targetTemperature += 0.5f
+            }
+        }) {
+            Icon(
+                painterResource(R.drawable.ic_plus),
+                contentDescription = null
+            )
+        }
+    }
 }
 
 @Composable
-fun RadialTemperatureControl(temperature: Temperature) {
-    Canvas(modifier = Modifier.fillMaxSize()) {
+fun RadialTemperatureDisplay(
+    temperature: Temperature
+) {
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(500.dp)
+    ) {
         val canvasWidth = size.width
         val canvasHeight = size.height
         val center = Offset(x = canvasWidth / 2, y = canvasHeight / 2)
-        val radius = (size.minDimension / 2)
+        val radius = (size.minDimension / 2.0f) //changing the divider here will change the size of the radial display
 
         val minTemperature = temperature.minTemperature
         val maxTemperature = temperature.maxTemperature
         val temperatureIncrements = temperature.increments
-        val numItems = ((maxTemperature - minTemperature) / temperatureIncrements) +1 // +1 to include an item for the max temperature
+        val numItems = ((maxTemperature - minTemperature) / temperatureIncrements) + 1 // +1 to include an item for the max temperature
         val sweepAngle = 185 // the degrees that we want to draw
         val angleIncrements = sweepAngle / numItems
 
@@ -151,18 +147,13 @@ fun RadialTemperatureControl(temperature: Temperature) {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+data class Temperature(
+    var targetTemperature: Float = 19f,
+    val minTemperature: Float = 12f,
+    val maxTemperature: Float = 28f,
+    val increments: Float = 0.5f
+) {
+    val isNotAtMinimum get() = targetTemperature != minTemperature
+    val isNotAtMaximum get() = targetTemperature != maxTemperature
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CustomControlsTheme {
-        Greeting("Android")
-    }
 }
